69a4abbdb150781f4f61dbe7f6b9b67f40f8dff0
Hipp01 <hippolarzul@gmail.com>
2024-11-15 11:12:15 +0100

    Fix pooling footer

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -2,12 +2,17 @@
   <div id="app">
     <router-view />
   </div>
+  <Footer />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Footer from './components/FooterComponent.vue';
 
 export default defineComponent({
   name: 'App',
+  components: {
+    Footer,
+  },
 });
 </script>
--- Frontend/src/views/OnboardingPage.vue
+++ Frontend/src/views/OnboardingPage.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
     <OnboardingComponent />
-    <FooterComponent />
   </div>
 </template>
 
 <script>
 import OnboardingComponent from '@/components/OnboardingComponent.vue';
-import FooterComponent from '@/components/FooterComponent.vue';
 
 export default {
   components: {
     OnboardingComponent,
-    FooterComponent,
   },
 };
 </script>
